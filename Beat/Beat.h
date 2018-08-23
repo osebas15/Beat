1814ab083b23bf2c1d3a3034e6e4c80eb4ff2cc5
Sebastian Aguirre <osebas15@gmail.com>
2018-08-23 14:25:31 -0400

    Initial Commit

---
+++ Beat/Beat.h
@@ -0,0 +1,19 @@
+//
+//  Beat.h
+//  Beat
+//
+//  Created by Sebastian Aguirre on 8/23/18.
+//  Copyright © 2018 SeaCap. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Beat.
+FOUNDATION_EXPORT double BeatVersionNumber;
+
+//! Project version string for Beat.
+FOUNDATION_EXPORT const unsigned char BeatVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Beat/PublicHeader.h>
+
+
