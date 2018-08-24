d5a5224e86b17c94196f3b19e2f083feee2cb5d0
Sebastian Aguirre <osebas15@gmail.com>
2018-08-23 22:15:05 -0400

    add the beat class

---
+++ Beat/Beat.swift
@@ -0,0 +1,66 @@
+//
+//  Beat.swift
+//  Beat
+//
+//  Created by Sebastian Aguirre on 8/23/18.
+//  Copyright © 2018 SeaCap. All rights reserved.
+//
+
+import Foundation
+
+class Beat {
+    
+    var action : ()->() = {}
+    
+    var numberOfActions : Int
+    
+    let queue : DispatchQueue
+    
+    let startDate : Date
+    
+    let delay : TimeInterval
+    
+    private var _on = false
+    
+    var on:Bool{
+        set{
+            if !_on && newValue{
+                _on = newValue
+                runningAction()
+            }
+            _on = newValue
+        }
+        get{
+            return _on
+        }
+    }
+    
+    required init(queue: DispatchQueue = DispatchQueue.main,
+                  delay: Double = 0.5,
+                  startDate: Date = Date(),
+                  numberOfActions: Int = -1,
+                  action: @escaping ()->() = {}) {
+        self.queue = queue
+        self.delay = delay
+        self.startDate = startDate
+        self.numberOfActions = numberOfActions
+        self.action = action
+    }
+    
+    func runningAction() {
+        
+        if numberOfActions != 0 && on{
+            self.action()
+            let popTime = DispatchTime.now() + Double(delay)
+        
+            numberOfActions -= 1
+            
+            queue.asyncAfter(deadline: popTime,
+                                        execute:
+            {
+                self.runningAction()
+            })
+            
+        }
+    }
+}
